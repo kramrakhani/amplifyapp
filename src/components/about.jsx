fc01539d3e37350778aeaa7375a34b28fc1b4ae9
kramrakhani <kramrakh@usc.edu>
2023-07-14 14:32:00 +0530

    portfolio app commit 2

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -13,8 +13,8 @@ export default class About extends Component {
                     <div className="about-desc">
                     <span className="heading-meta">About Us</span>
                     <h2 className="colorlib-heading">Who Am I?</h2>
-                    <p>I am a computer grad student from DAIICT-Gandhinagar with ICT as my major.I love exploring new technologies and often amazed by the progress we as a human species have mad so far in the recent years (apart from the headphone-jack part!!)</p>
-                    <p>I have started reflecting my ideas and thougths through the medium of words recently so spelling and grammer mistaks are very often.You can write me back if you spot any and don't want to live anymore :P </p>
+                    <p>I majored in Computer Science and Business Administration from the University of Southern California</p>
+                    <p>More to follow.... </p>
                     </div>
                 </div>
                 </div>
